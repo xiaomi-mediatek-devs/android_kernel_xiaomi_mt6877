f1e80217dfc42a5f5bee9e8fe1a4d48a4bf122cd
Ankit Goyal <layog@google.com>
2021-07-13 17:21:51 +0800

    mali_kbase: platform: Add per-process and global sysfs nodes for GPU mem usage
    Bug: 191966412
    Signed-off-by: Ankit Goyal <layog@google.com>
    Change-Id: Id47feadaf9da7ef8e22494ab64e6263d7f87213c

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_defs.h
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_defs.h
@@ -639,6 +639,8 @@ struct kbase_devfreq_queue_info {
  *                      Used to ensure that pages of allocation are accounted
  *                      only once for the process, even if the allocation gets
  *                      imported multiple times for the process.
+ * @kobj:               Links to the per-process sysfs node
+ *                      &kbase_device.proc_sysfs_node.
  */
 struct kbase_process {
 	pid_t tgid;
@@ -648,6 +650,8 @@ struct kbase_process {
 
 	struct rb_node kprcs_node;
 	struct rb_root dma_buf_root;
+
+	struct kobject kobj;
 };
 
 /**
@@ -958,6 +962,7 @@ struct kbase_process {
  * @pcm_dev:                The priority control manager device.
  * @oom_notifier_block:     notifier_block containing kernel-registered out-of-
  *                          memory handler.
+ * @proc_sysfs_node:        Sysfs directory node to store per-process stats.
  */
 struct kbase_device {
 	u32 hw_quirks_sc;
@@ -1220,6 +1225,7 @@ struct kbase_device {
 	struct job_status_qos job_status_addr;
 	struct v1_data* v1;
 #endif
+    struct kobject *proc_sysfs_node;
 };
 
 /**
