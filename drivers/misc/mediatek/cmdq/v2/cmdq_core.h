abddd240887ea38e6ba1378bc4421bf7d7f5af8b
LinkBoi00 <linkdevel@protonmail.com>
2023-01-01 11:52:55 +0200

    drivers: misc/mtk: cmdq/mdp: Tone down debug and verbose logging
    The increased debug log printing from CMDQ_LOG causes noticeable UX lag
    
    Change-Id: Ia349423569bde05529554349e71c9aad9ce178e9
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- drivers/misc/mediatek/cmdq/v2/cmdq_core.h
+++ drivers/misc/mediatek/cmdq/v2/cmdq_core.h
@@ -136,28 +136,22 @@ struct DumpFirstErrorStruct {
 
 #define CMDQ_LOG(string, args...) \
 do {			\
-	pr_notice("[CMDQ]"string, ##args); \
-	cmdq_core_save_first_dump("[CMDQ]"string, ##args); \
+	pr_debug("[CMDQ]"string, ##args); \
 } while (0)
 
 #define CMDQ_MSG(string, args...) \
 do {			\
-	if (cmdq_core_should_print_msg()) { \
-		pr_notice("[CMDQ]"string, ##args); \
-}			\
+	pr_debug("[CMDQ]"string, ##args); \
 } while (0)
 
 #define CMDQ_VERBOSE(string, args...) \
 do {			\
-	if (cmdq_core_should_print_msg()) { \
-		pr_debug("[CMDQ]"string, ##args); \
-}			\
+	pr_debug("[CMDQ]"string, ##args); \
 } while (0)
 
 #define CMDQ_ERR(string, args...) \
 do {			\
-	pr_notice("[CMDQ][ERR]"string, ##args); \
-	cmdq_core_save_first_dump("[CMDQ][ERR]"string, ##args); \
+	pr_err("[CMDQ][ERR]"string, ##args); \
 } while (0)
 
 #ifdef CMDQ_AEE_READY
@@ -189,8 +183,6 @@ do {			\
 	snprintf(dispatchedTag, 50, "CRDISPATCH_KEY:%s", tag); \
 	pr_debug("[CMDQ][AEE] AEE not READY!!!"); \
 	pr_debug("[CMDQ][AEE]"string, ##args); \
-	cmdq_core_save_first_dump("[CMDQ][AEE]"string, ##args); \
-	cmdq_core_turnoff_first_dump(); \
 } while (0);	\
 }
 #endif
