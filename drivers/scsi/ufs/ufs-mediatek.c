6de563039492abdb78c3360062eeec965ab68a78
CloudedQuartz <ayan.pande06@gmail.com>
2023-06-03 10:40:07 +0530

    drivers: scsi: ufs-mediatek: add missing function prototype
    ../drivers/scsi/ufs/ufs-mediatek.c:110:42: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    struct rpmb_dev *ufs_mtk_rpmb_get_raw_dev()
                                             ^
                                              void
    1 error generated.
    
    Change-Id: I6abcb7e9156ffe6da3e30498b49f0097b3041e9a
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -107,7 +107,7 @@ static const struct of_device_id ufs_mtk_of_match[] = {
 extern bool clk_buf_ctrl(enum clk_buf_id id, bool onoff);
 #endif
 
-struct rpmb_dev *ufs_mtk_rpmb_get_raw_dev()
+struct rpmb_dev *ufs_mtk_rpmb_get_raw_dev(void)
 {
 	struct ufs_mtk_host *host = ufshcd_get_variant(ufs_mtk_hba);
 
