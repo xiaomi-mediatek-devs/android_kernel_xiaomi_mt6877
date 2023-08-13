2c0fbdfc81bce14132ec94044e55842879d07bfe
bengris32 <bengris32@protonmail.ch>
2023-08-13 23:11:38 +0100

    Revert "[ALPS07034142] Spectre-BHB: CVE-2022-23960"
    * Reason: Spectre mitigations are merged in ACK.
    
    This reverts commit f55d7b567a593ef980b3a698cf4a1b931304ae50.
    
    Change-Id: Ib07f777020b714542f249a42e5f1caef8239fece

--- arch/arm64/kvm/hyp/switch.c
+++ arch/arm64/kvm/hyp/switch.c
@@ -34,7 +34,6 @@
 #include <asm/debug-monitors.h>
 #include <asm/processor.h>
 #include <asm/thread_info.h>
-#include <asm/vectors.h>
 
 extern struct exception_table_entry __start___kvm_ex_table;
 extern struct exception_table_entry __stop___kvm_ex_table;
@@ -156,13 +155,10 @@ static void __hyp_text __activate_traps(struct kvm_vcpu *vcpu)
 
 static void deactivate_traps_vhe(void)
 {
-	const char *host_vectors = vectors;
+	extern char vectors[];	/* kernel exception vectors */
 	write_sysreg(HCR_HOST_VHE_FLAGS, hcr_el2);
 	write_sysreg(CPACR_EL1_DEFAULT, cpacr_el1);
-
-	if (!arm64_kernel_unmapped_at_el0())
-		host_vectors = __this_cpu_read(this_cpu_vector);
-	write_sysreg(host_vectors, vbar_el1);
+	write_sysreg(vectors, vbar_el1);
 }
 NOKPROBE_SYMBOL(deactivate_traps_vhe);
 
