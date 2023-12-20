03532b8a74d3bbd4d6760807c1b8b9aa846c71db
bengris32 <bengris32@protonmail.ch>
2023-12-20 00:07:15 +0000

    gen4m: Guard more wakelock code with `CFG_ENABLE_WAKE_LOCK`
    Change-Id: If2ab2d439256e9dc423e4de34c0f347a6134c860
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_rst.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_rst.c
@@ -1073,8 +1073,10 @@ int wlan_reset_thread_main(void *data)
 #endif
 		WIPHY_PRIV(wlanGetWiphy(), prGlueInfo);
 		if (test_and_clear_bit(GLUE_FLAG_RST_START_BIT, &g_ulFlag)) {
+#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 			if (KAL_WAKE_LOCK_ACTIVE(NULL, g_IntrWakeLock))
 				KAL_WAKE_UNLOCK(NULL, g_IntrWakeLock);
+#endif
 
 			if (g_IsWholeChipRst) {
 #if (CFG_ANDORID_CONNINFRA_COREDUMP_SUPPORT == 1)
@@ -1115,8 +1117,10 @@ int wlan_reset_thread_main(void *data)
 				g_SubsysRstTotalCnt);
 		}
 		if (test_and_clear_bit(GLUE_FLAG_RST_END_BIT, &g_ulFlag)) {
+#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 			if (KAL_WAKE_LOCK_ACTIVE(NULL, g_IntrWakeLock))
 				KAL_WAKE_UNLOCK(NULL, g_IntrWakeLock);
+#endif
 			DBGLOG(INIT, INFO, "Whole chip reset end start\n");
 			glResetMsgHandler(WMTMSG_TYPE_RESET,
 				WMTRSTMSG_RESET_END);
